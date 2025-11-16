97fa08fb63295eea5615b8e462dcff46a86a80df
Michael Dickinson <michael.dickinson3949@gmail.com>
2025-11-15 17:44:01 -0800

    use radix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "@radix-ui/themes/styles.css";
 import App from './App.tsx'
+import { Theme } from '@radix-ui/themes/dist/cjs/components/index.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </StrictMode>,
 )
